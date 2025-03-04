ede927a0057ae99ea6227f1c22da3538edc7846e
shruti985 <jainshruti753@gmail.com>
2025-03-04 11:11:38 +0530

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,26 @@
+LeetCode POTD:https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/
+04/03/2025
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        int num=n,i=-1,sum=0;
+        while(num>0){
+            num=num/3;
+            i++;           
+        } 
+        while(i>=0){
+            if(sum+(int)Math.pow(3,i)<=n){
+                sum=sum+(int)Math.pow(3,i);
+                i--;
+            }
+            else{
+                i--;
+            }
+        }
+        if(sum==n){
+            return true;
+        }
+        return false;
+    }
+}
+SC:O(1)
+TC:O(logn)
