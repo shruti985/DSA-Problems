e3942b74433f40adf37cf9030d3baf33b7011f33
shruti985 <jainshruti753@gmail.com>
2025-03-09 13:56:48 +0530

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -1,26 +1,40 @@
+//Problem 1:Move zeroes
+//https://leetcode.com/problems/move-zeroes/
+//Approach:Traversed the nums array and stored the index of zero in k and whenever a non-zero no. was found swapped it with zero at index k and incremented k.
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0, right = 1, index = 1, k = 0, temp;
-        while (right < nums.length) {
-            if (nums[left] == 0 && nums[right] != 0) {
-                if (k == 1) {
-                    temp = nums[index];
-                    nums[index] = nums[right];
-                    nums[right] = temp;
-                    index++;
-                } else {
-                    temp = nums[left];
-                    nums[left] = nums[right];
-                    nums[right] = temp;
-                }
-
-            } else if (nums[right] == 0 && nums[left] == 0 && k == 0) {
-                index = left;
-                k = 1;
+        int k=0,i=0,temp;
+        while(i<nums.length){
+            if(nums[i]!=0){
+                temp=nums[k];
+                nums[k]=nums[i];
+                nums[i]=temp;
+                k++;
             }
-            left++;
-            right++;
+            i++;
         }
-
     }
 }
+//TC:O(N)
+//SC:O(1)
+
+//Problem 2:Rotate-array
+https://leetcode.com/problems/rotate-array/
+class Solution {
+    public void reverse(int[] nums,int l,int r){
+       while(l<r){
+        int temp=nums[l];
+        nums[l]=nums[r];
+        nums[r]=temp;
+        l++;r--;
+       }
+    }
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }   
+}
+//TC:O(N)
+//SC:O(1)
